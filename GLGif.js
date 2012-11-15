6c4d918497768a9112fbe003213afaf94d2adcfb
Jordi Ros <shine.3p@gmail.com>
2012-11-15 23:45:08 +0100

    First version

---
+++ GLGif.js
@@ -0,0 +1,96 @@
+//--------------------------------------------------------------------------------------------------------
+// @author Jordi Ros: shine.3p@gmail.com
+//
+// GLGif.js
+//
+//--------------------------------------------------------------------------------------------------------
+
+function loadGif(img, onload) {
+	// Init
+	var gif = {};
+	gif.tex = [];
+	gif.frame = 0;
+	gif.loaded = false;
+	gif.speed = 10;
+	gif.time = 0;
+	gif.reset = function(time) {
+		this.time = 0;
+	}
+	gif.setSpeed = function(fps) {
+		this.speed = fps;
+	}
+	gif.update = function(time) {
+		this.frame = Math.round(((time - this.time) * this.speed) % (this.tex.length-1));
+	};
+	gif.getTexture = function() {
+		return this.tex[this.frame];
+	}
+
+	// loadImage
+	var canvas, ctx;
+	var hdr;
+	var tcanvas;
+
+	// Do nothing
+	var doNothing = function() {};
+
+	// Do header
+	var doHdr = function(_hdr) {
+		hdr = _hdr;
+	};
+	
+	var doGCE = function(gce) {
+		canvas = document.createElement('canvas');
+		canvas.width = hdr.width;
+		canvas.height = hdr.height;				
+		ctx = canvas.getContext('2d');				
+	};
+
+	// Do Image
+	var doImg = function(img) {
+		// Load pixels
+		var ct = img.lctFlag ? img.lct : hdr.gct;
+		var cData = ctx.getImageData(img.leftPos, img.topPos, img.width, img.height);
+		img.pixels.forEach(function(pixel, i) {
+			cData.data[i * 4 + 0] = ct[pixel][0];
+			cData.data[i * 4 + 1] = ct[pixel][1];
+			cData.data[i * 4 + 2] = ct[pixel][2];
+			cData.data[i * 4 + 3] = 255;
+		});
+		ctx.putImageData(cData, img.leftPos, img.topPos);
+		ctx.drawImage(canvas, 0, 0, img.width, img.height);
+
+		// Create THREE texture
+		var texture = new THREE.Texture(canvas);
+		texture.needsUpdate = true;
+		gif.tex.push(texture);
+	};
+
+	var doEof = function(block) {
+		gif.loaded = true;
+		gif.reset(0);
+		if (onload)
+			onload();
+	}
+
+	// Gif Handler
+	var gifHandler = {
+		hdr: doHdr,
+		gce: doGCE,
+		com: doNothing,
+		app: { NETSCAPE: doNothing },
+		img: doImg,
+		eof: doEof,
+	};
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", img, true);
+	xhr.overrideMimeType('text/plain; charset=x-user-defined');
+	xhr.onload = function(e) {
+		var buffer = xhr.responseText;
+		parseGIF(new Stream(buffer), gifHandler);
+	};
+	xhr.send(null);
+
+	return gif;
+}
--- README.md
+++ README.md
@@ -2,3 +2,8 @@ GLGif
 =====
 
 Three.js Gif material loader
+
+Watch it live for a usage example
+
+http://www.jordiros.me/glgifomatic/gangnam.html
+http://www.jordiros.me/glgifomatic/napoleon.html
